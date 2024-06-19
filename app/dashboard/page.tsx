48c83e4ca3bae1d452a01019508fca20da0003d4
Mabior-Graham <mabiorgraham48@gmail.com>
2024-06-19 08:56:50 -0700

    dashboard updated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,62 @@
 "use client"
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+  Center,
+  Heading,
+} from '@chakra-ui/react'
+
 export default function page(){
   return(
     <div>
-      Dashboard
+     <div className='m-4'>
+     <Heading size={'lg'}>Dummy Stats</Heading>
+     </div>
+        
+            <StatGroup>
+        <Stat>
+          <StatLabel>Sent</StatLabel>
+          <StatNumber>345,670</StatNumber>
+          <StatHelpText>
+            <StatArrow type='increase' />
+            23.36%
+          </StatHelpText>
+        </Stat>
+
+        <Stat>
+          <StatLabel>Clicked</StatLabel>
+          <StatNumber>45</StatNumber>
+          <StatHelpText>
+            <StatArrow type='decrease' />
+            9.05%
+          </StatHelpText>
+        </Stat>
+      </StatGroup>
+      <div className='m-4'></div>
+      <StatGroup>
+        <Stat>
+          <StatLabel>Sent</StatLabel>
+          <StatNumber>345,670</StatNumber>
+          <StatHelpText>
+            <StatArrow type='increase' />
+            23.36%
+          </StatHelpText>
+        </Stat>
+
+        <Stat>
+          <StatLabel>Clicked</StatLabel>
+          <StatNumber>45</StatNumber>
+          <StatHelpText>
+            <StatArrow type='decrease' />
+            9.05%
+          </StatHelpText>
+        </Stat>
+      </StatGroup>
+      
     </div>
   )
 }
